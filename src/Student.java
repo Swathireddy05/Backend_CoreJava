063d781b94603488c0d2cb6d5cffcdea90ae13e9
Swathireddy05 <swathireddysurikanti@gmail.com>
2023-05-31 18:29:40 -0500

    commit2

---
+++ src/Student.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Student {
+    static String name="Swathi";
+    public static String getName()
+    {
+        return name;
+    }
+    public static void setName(String name)
+    {
+        name=name;
+
+    }
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Name:"+getName());
+        //SETTING THE NEW NAME
+        name = sc.next();
+        setName(name);
+        System.out.println("Updated New Name:"+getName());
+
+
+    }
+
+}
+
