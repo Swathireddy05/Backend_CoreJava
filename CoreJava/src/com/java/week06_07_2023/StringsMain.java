be2317ca38ddae14346e591a254a3955fa9432f6
Swathireddy05 <swathireddysurikanti@gmail.com>
2023-06-09 12:15:00 -0500

    commit

---
+++ CoreJava/src/com/java/week06_07_2023/StringsMain.java
@@ -0,0 +1,24 @@
+package com.java.week06_07_2023;
+
+import java.util.Scanner;
+
+public class StringsMain {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String name=sc.next();
+
+        //Checking for anagram
+        Anagram a=new Anagram();
+        String name1=sc.next();
+        String name2=sc.next();
+        System.out.println("Is Anagram " + a.FindAnagram(name1,name2));
+
+        //reversing words in a string
+        reverse sr=new reverse();
+        sr.stringRev("Java J2EE Reverse Me");
+
+        //isrotation or not
+        rotation ss=new rotation();
+        System.out.println("Is rotation "+ss.isRotation("JavaJ2eeStrutsHibernate","StrutsHibernateJavaJ2ee"));
+    }
+}
