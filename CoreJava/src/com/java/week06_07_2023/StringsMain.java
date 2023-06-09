a4a18c226d8eb6ff68f31e29f43138149cbaea6b
Swathireddy05 <swathireddysurikanti@gmail.com>
2023-06-09 15:42:52 -0500

    commit

--- CoreJava/src/com/java/week06_07_2023/StringsMain.java
+++ CoreJava/src/com/java/week06_07_2023/StringsMain.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class StringsMain {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        String name=sc.next();
 
         //Checking for anagram
         Anagram a=new Anagram();
